7f039ca7163985ecdfff2dcfd9af8b09f473e023
Titarahmadani <150914705+Titarahmadani@users.noreply.github.com>
2024-03-09 16:39:00 +0700

    Add files via upload

---
+++ aplikasikotlin/app/src/test/java/com/example/aplikasikotlin/Aquarium.kt
@@ -0,0 +1,24 @@
+package com.example.aplikasikotlin.myapp
+
+class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} liters")
+    }
+
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+    }
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        // calculate the height needed
+        height = (tank / (length * width)).toInt()
+    }
+
+}
---
+++ aplikasikotlin/app/src/test/java/com/example/aplikasikotlin/Main.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasikotlin.myapp
+
+fun buildAquarium() {
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
+}
+
+fun main() {
+    buildAquarium()
+}
